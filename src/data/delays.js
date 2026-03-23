4d50892de0ffc1e2df03a1535a17b252a2e387b8
Jack Jona <65679392+jackjona@users.noreply.github.com>
2026-03-23 03:41:58 +0000

    add subtle animations

---
+++ src/data/delays.js
@@ -0,0 +1,9 @@
+export const ANIMATION_DELAYS = [
+  "motion-delay-100",
+  "motion-delay-200",
+  "motion-delay-300",
+  "motion-delay-400",
+  "motion-delay-500",
+  "motion-delay-500",
+  "motion-delay-600",
+];
